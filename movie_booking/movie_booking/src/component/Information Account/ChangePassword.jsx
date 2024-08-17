f3e17d1a8bba3e05ef3506d9ed01ab8dca0440f5
NguyenLocHoangGia <hoanggia09052004@gmail.com>
2024-08-17 21:42:08 +0700

    push FE

--- movie_booking/movie_booking/src/component/Information Account/ChangePassword.jsx
+++ movie_booking/movie_booking/src/component/Information Account/ChangePassword.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { toast } from 'react-toastify';
 import axios from 'axios';
+import Sidebar from "./Sidebar";
 
 const ChangePassword = () => {
     const [currentPassword, setCurrentPassword] = useState('');
@@ -42,56 +43,62 @@ const ChangePassword = () => {
 
 
     return (
-
-        <div className="p-4">
-            <h2 className="text-xl font-bold mb-4">Thay đổi mật khẩu</h2>
-            <div className="mb-4">
-                <label htmlFor="oldPassword" className="block text-sm font-medium text-gray-700">
-                    Mật khẩu cũ
-                </label>
-                <input
-                    type="password"
-                    id="oldPassword"
-                    value={currentPassword}
-                    onChange={(e) => setCurrentPassword(e.target.value)}
-                    className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                    required
-                />
+        <div className="max-w-[1170px] min-h-[calc(100vh-69px)] mx-auto px-4 py-6">
+            <div className="flex flex-row gap-12 justify-start items-start">
+                <Sidebar/>
+                <div className="w-full min-h-screen py-1 md:w-2/3 lg:w-3/4">
+                    <div className="p-2 md:p-4 bg-white rounded-lg shadow-md">
+                        <h2 className="text-xl font-bold mb-4">Thay đổi mật khẩu</h2>
+                        <div className="mb-4">
+                            <label htmlFor="currentPassword" className="block text-sm font-medium text-gray-700">
+                                Mật khẩu cũ
+                            </label>
+                            <input
+                                type="password"
+                                id="currentPassword"
+                                value={currentPassword}
+                                onChange={(e) => setCurrentPassword(e.target.value)}
+                                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                required
+                            />
+                        </div>
+                        <div className="mb-4">
+                            <label htmlFor="newPassword" className="block text-sm font-medium text-gray-700">
+                                Mật khẩu mới
+                            </label>
+                            <input
+                                type="password"
+                                id="newPassword"
+                                value={newPassword}
+                                onChange={(e) => setNewPassword(e.target.value)}
+                                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                required
+                            />
+                        </div>
+                        <div className="mb-4">
+                            <label htmlFor="confirmationPassword" className="block text-sm font-medium text-gray-700">
+                                Xác nhận mật khẩu mới
+                            </label>
+                            <input
+                                type="password"
+                                id="confirmationPassword"
+                                value={confirmationPassword}
+                                onChange={(e) => setConfirmationPassword(e.target.value)}
+                                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                required
+                            />
+                        </div>
+                        <button
+                            type="button"
+                            onClick={handleSaveClick}
+                            className={`mt-4 inline-flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ${loading ? 'opacity-50 cursor-not-allowed' : ''}`}
+                            disabled={loading}
+                        >
+                            {loading ? 'Đang cập nhật...' : 'Lưu'}
+                        </button>
+                    </div>
+                </div>
             </div>
-            <div className="mb-4">
-                <label htmlFor="newPassword" className="block text-sm font-medium text-gray-700">
-                    Mật khẩu mới
-                </label>
-                <input
-                    type="password"
-                    id="newPassword"
-                    value={newPassword}
-                    onChange={(e) => setNewPassword(e.target.value)}
-                    className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                    required
-                />
-            </div>
-            <div className="mb-4">
-                <label htmlFor="confirmPassword" className="block text-sm font-medium text-gray-700">
-                    Xác nhận mật khẩu mới
-                </label>
-                <input
-                    type="password"
-                    id="confirmPassword"
-                    value={confirmationPassword}
-                    onChange={(e) => setConfirmationPassword(e.target.value)}
-                    className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                    required
-                />
-            </div>
-            <button
-                type="button"
-                onClick={handleSaveClick}
-                className={`mt-4 inline-flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ${loading ? 'opacity-50 cursor-not-allowed' : ''}`}
-                disabled={loading}
-            >
-                {loading ? 'Đang cập nhật...' : 'Lưu'}
-            </button>
         </div>
     );
 };
